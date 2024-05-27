784d47abb752107257646a565c6ee789582a8119
Adriel Rosa <minilord002@gmail.com>
2024-05-27 01:22:20 -0300

    SECOND UPDATE

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -27,8 +27,14 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	var direction := 1
+	velocity.x = direction * SPEED
+	animation.scale.x = direction
+
 	"""
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction
@@ -40,4 +46,5 @@ func _physics_process(delta):
 		animation.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	"""
+	
 	move_and_slide()
